3348a1aab450eda389fa3494033a301899a3a706
arpan2004 <56141180+arpan2004@users.noreply.github.com>
2024-02-15 15:59:28 -0500

    Add files via upload

--- AlternateEvenCase.py
+++ AlternateEvenCase.py
@@ -4,10 +4,11 @@ from geometry_tools import drawtools
 
 
 Degree = 6  # degree of the tree
-Degree_Alt = 6
+Degree_Alt = 4
 SegmentLength = np.cos(np.pi/Degree) # branch length
-Depth = 6  # number of iterations
-Generators = [] # stores the generators for the group associated with the tree. Generators are represented by mobius transforms as 2x2 complex valued matricies
+Depth = 2  # number of iterations
+Generators_even = dict() # stores the generators for the group associated with the tree. Generators are represented by mobius transforms as 2x2 complex valued matricies
+Generators_odd = dict()
 Geodesics = [] # keeps track of edges/geodesics of tree
 Points = [] # array of all points/verticies in tree
 Origin = np.array([0,1])
@@ -26,27 +27,29 @@ def FindGenerators(Generators, Degree):    # this function produces the list of 
             else:
                 rot = np.clongdouble(np.exp((1j*np.pi*(i))/(Degree / 2))) #lower case zeta in the original paper page 317
             gen = np.array([[1,SegmentLength * rot],[SegmentLength * np.conjugate(rot),1]])
-            Generators.append(gen)
+            Generators[i] = gen
         else:    # odd degree case
             rot = np.clongdouble(np.exp((1j * np.pi * (2*i-1)) / (Degree)))
             gen = np.array([[SegmentLength*rot,-1],[1,(-1)*SegmentLength*np.conjugate(rot)]])
-            Generators.append(gen)
+            Generators[i] = gen
         
     return Generators
 
 def Norm_Vector(vector):
     return vector[0] / vector[1]
 
+def Find_Gen_Inverse(generator_index, total_gens):
+    return (generator_index + (total_gens/2)) % total_gens
 
-
-def FindVerticies(parentwords, parentc, backwardgens, currentDepth):  # function produces array of all verticies from the generators
+def FindVerticies(Generators, parentwords, parentc, backwardgens, currentDepth):  # function produces array of all verticies from the generators
     newparentwords = [] #new words to be added to the associated group IE compositions of generators
     newparentc = [] #new verticies as points in C
     newbackwardgens = [] #the generator that would take a given parent backwards. Indices need to be identical to newparentwords TODO implement this as dictionary
     for k, parent in enumerate(parentwords):
         parentPoint = hyperbolic.Point([parentc[k].real, parentc[k].imag], model=Model)
         if EvenDegree:    # even degree case
-            for i, gen in enumerate(Generators):
+            for i in Generators_even:
+                gen = Generators_even[i]
                 if (i != backwardgens[k]) or (backwardgens[k] == -1): #ignore the generator that would take us backwards toward the origin
                     word = parent@gen #get the next word by composing the current generator with the parent word
                     z = word@Origin #multiply origin by the new word to find the coordinate of the vertex in projective space
@@ -75,45 +78,8 @@ def FindVerticies(parentwords, parentc, backwardgens, currentDepth):  # function
 
     #base case
     if currentDepth < Depth:
-        FindVerticies(newparentwords,newparentc,newbackwardgens,currentDepth+1)
+        FindVerticies(Generators, newparentwords,newparentc,newbackwardgens,currentDepth+1)
 
-def FindVerticies_Alt(parentwords, parentc, backwardgens, currentDepth):  # function produces array of all verticies from the generators
-    newparentwords = [] #new words to be added to the associated group IE compositions of generators
-    newparentc = [] #new verticies as points in C
-    newbackwardgens = [] #the generator that would take a given parent backwards. Indices need to be identical to newparentwords TODO implement this as dictionary
-    for k, parent in enumerate(parentwords):
-        parentPoint = hyperbolic.Point([parentc[k].real, parentc[k].imag], model=Model)
-        if EvenDegree:    # even degree case
-            for i, gen in enumerate(Generators):
-                if (i != backwardgens[k]) or (backwardgens[k] == -1): #ignore the generator that would take us backwards toward the origin
-                    word = parent@gen #get the next word by composing the current generator with the parent word
-                    z = word@Origin #multiply origin by the new word to find the coordinate of the vertex in projective space
-                    z = z[0] / z[1] #convert from projective space to C
-                    newparentwords.append(word)
-                    newbackwardgens.append((i + (Degree_Alt/2)) % Degree_Alt) #the index of the inverse generator for the generator just applied
-                    newparentc.append(z) #save parent as complex number
-                    childPoint= hyperbolic.Point([z.real, z.imag], model=Model) #need to convert to special class for geometry_tools
-                    Points.append(childPoint)
-                    geodesic = hyperbolic.Segment(parentPoint,childPoint)
-                    Geodesics.append(geodesic)
-        else:
-            #TODO ignore generators that take us backwards
-            for i, gen in enumerate(Generators):  #odd degree case
-                word = parent @ gen
-                z = word @ Origin
-                z = z[0] / z[1]
-                newparentwords.append(word)
-                newbackwardgens.append(i)
-                newparentc.append(z)
-                childPoint = hyperbolic.Point([z.real, z.imag], model=Model)
-                Points.append(childPoint)
-                geodesic = hyperbolic.Segment(parentPoint, childPoint)
-                Geodesics.append(geodesic)
-
-
-    #base case
-    if currentDepth < Depth:
-        FindVerticies(newparentwords,newparentc,newbackwardgens,currentDepth+1)
 
 def Render(color):    # calls geometry tools to render the hyperbolic tree
     figure = drawtools.HyperbolicDrawing(model=Model)
@@ -126,8 +92,8 @@ def Render(color):    # calls geometry tools to render the hyperbolic tree
 
 
 #Main
-Generators = FindGenerators([], Degree)
-FindVerticies([Generators[0]],[Norm_Vector(Generators[0]@np.array([0+0j,1+0j]))],[4],0)
+Generators_even = FindGenerators(dict(), Degree)
+FindVerticies(Generators_even, [Generators_even[0]],[Norm_Vector(Generators_even[0]@np.array([0+0j,1+0j]))],[Find_Gen_Inverse(0, len(Generators_even))], 0)
 Render('RED')
 # Generators = FindGenerators([], Degree_Alt)
 # #Generators.append(FindGenerators([], 4))
