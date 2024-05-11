6bd48d20f7c807065b501ffbb91500a077aabf3f
Connor.Poulton <poulton.cw@gmail.com>
2024-05-11 15:39:50 -0400

    Added comments explaining the math behind the code, DEGREE and TOTAL_ITERATIONS are now defined by passing in variables rather than being hardcoded.

--- HomogenusCase.py
+++ HomogenusCase.py
@@ -1,10 +1,11 @@
+import sys
 import numpy as np
 from geometry_tools import hyperbolic
 from geometry_tools import drawtools
 
 
-DEGREE = 6
-TOTAL_ITERATIONS = 5
+DEGREE = int(sys.argv[1])
+TOTAL_ITERATIONS = int(sys.argv[2])
 MODEL = hyperbolic.Model.POINCARE
 SEGMENT_LENGTH = np.cos(np.pi/DEGREE)
 ORIGIN = np.array([0, 1])
@@ -15,15 +16,15 @@ Geodesics = []
 
 class VertexWord:
     def __init__(self, projection_matrix, backward_projection_matrix):
-        self.proj_mtrx = projection_matrix
-        self.bk_proj_mtrx = backward_projection_matrix
+        self.proj_mtrx = projection_matrix #matrix representation of the disk automorphisim associated with the vertex
+        self.bk_proj_mtrx = backward_projection_matrix #matrix associated with the inverse generator of this word's rightmost generator (IE the generator that takes us backwards)
 
 
-def find_generators():
+def find_generators():#find the matrix associated with each generator of the free group associated with the given degree, then append it's inverse generator.
     generators = []
     for i in range(0, DEGREE):
         angle_of_rot = np.exp((1j*np.pi*i) / (DEGREE / 2))
-        generator = np.array([[1, SEGMENT_LENGTH*angle_of_rot], [SEGMENT_LENGTH*np.conjugate(angle_of_rot), 1]])
+        generator = np.array([[1, SEGMENT_LENGTH*angle_of_rot], [SEGMENT_LENGTH*np.conjugate(angle_of_rot), 1]]) #see Colona's paper on hyperbolic tree embeddings, page 317 for the derivation of this formula
         generators.append(generator)
 
     for i, gens in enumerate(generators):
@@ -32,15 +33,15 @@ def find_generators():
         Generators.append(VertexWord(gens, generators[inverse_id]))
 
 
-def normalize_projective_vector(proj_coordinate):
+def normalize_projective_vector(proj_coordinate): #convert from points in CP1 to points in the complex plane
     return proj_coordinate[0] / proj_coordinate[1]
 
 
-def generate_tree(parent_words, iterations):
+def generate_tree(parent_words, iterations): #iterative right actions by each generator
     current_words = []
     for parent in parent_words:
         for gens in Generators:
-            if not np.array_equal(gens.bk_proj_mtrx, parent.bk_proj_mtrx):
+            if not np.array_equal(gens.bk_proj_mtrx, parent.bk_proj_mtrx): #ignore the generator that sends us backwards
                 new_word = parent.proj_mtrx @ gens.proj_mtrx
                 current_words.append(VertexWord(new_word, gens.bk_proj_mtrx))
                 child_projective_coordinates = new_word @ ORIGIN
